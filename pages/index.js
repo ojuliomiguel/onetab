c6556b0dbd88f88495191a5af28cf201e0f3c442
Júlio Miguel <37316841+ojuliomiguel@users.noreply.github.com>
2023-10-21 12:38:44 +0000

    feat: generating first impact

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import React, { useState } from 'react';
+
+function ClickButton({ onClick }) {
+    return (
+        <button onClick={onClick}>
+            Click and see the magic
+        </button>
+    );
+}
+
 function Home() {
-    return <h1>Tap To News</h1>
+    const handleClick = () => {
+        setShowImage(!showImage);
+    };
+
+    const [showImage, setShowImage] = useState(false);
+
+    return (
+        <div>
+            <ClickButton onClick={handleClick}>
+                Click and see the magic
+            </ClickButton>
+
+            {showImage && (
+                <img
+                    src="https://i.ibb.co/dBr02DF/DALL-E-2023-10-21-09-31-30-Vector-image-of-a-chemistry-teacher-with-curly-hair-featuring-golden-high.png"
+                    alt="Descrição da imagem"
+                />
+            )}
+        </div>
+    );
 }
 
 export default Home;
