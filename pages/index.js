5fe67fa8d8429d0ddb579116546522ac303f2183
Júlio Miguel <37316841+ojuliomiguel@users.noreply.github.com>
2023-11-23 01:43:54 +0000

    chore: adiciona spcripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function ClickButton({ onClick }) {
-    return (
-        <button onClick={onClick}>
-            Click and see the magic
-        </button>
-    );
+  return <button onClick={onClick}>Click and see the magic</button>;
 }
 
 function Home() {
-    const handleClick = () => {
-        setShowImage(!showImage);
-    };
+  const handleClick = () => {
+    setShowImage(!showImage);
+  };
 
-    const [showImage, setShowImage] = useState(false);
+  const [showImage, setShowImage] = useState(false);
 
-    return (
-        <div>
-            <ClickButton onClick={handleClick}>
-                Click and see the magic
-            </ClickButton>
+  return (
+    <div>
+      <ClickButton onClick={handleClick}>Click and see the magic</ClickButton>
 
-            {showImage && (
-                <img
-                    src="https://i.ibb.co/dBr02DF/DALL-E-2023-10-21-09-31-30-Vector-image-of-a-chemistry-teacher-with-curly-hair-featuring-golden-high.png"
-                    alt="Descrição da imagem"
-                />
-            )}
-        </div>
-    );
+      {showImage && (
+        <img
+          src="https://i.ibb.co/dBr02DF/DALL-E-2023-10-21-09-31-30-Vector-image-of-a-chemistry-teacher-with-curly-hair-featuring-golden-high.png"
+          alt="Descrição da imagem"
+        />
+      )}
+    </div>
+  );
 }
 
 export default Home;
